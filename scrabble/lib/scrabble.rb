b10eaf60aa239ce55eab2502d1cc075138c21aac
Amit Tauro <tauroamit@gmail.com>
2020-07-21 19:55:36 +0100

    30 mins

--- scrabble/lib/scrabble.rb
+++ scrabble/lib/scrabble.rb
@@ -5,8 +5,15 @@ class Scrabble
   end
 
   def score
-    {"AEIOULNRST" => 1, "DG" => 2,"BCMP" =>	3, "FHVWY" =>4, "K" => 5, "JX" =>8, "QZ" => 10}
-    @string
+    list = {"AEIOULNRST" => 1, "DG" => 2,"BCMP" =>	3, "FHVWY" =>4, "K" => 5, "JX" =>8, "QZ" => 10}
+    #first letter is C
+    #C is included in BCMP so that means C is = 3
+    strings = list.keys
+    puts "strings #{strings}"
+    puts "find letter A #{strings.select{ |x| x.include?("A")}}"
+    findingkey = strings.select{ |x| x.include?("A")}
+    value = list.fetch(findingkey)
+    puts "actual string #{@string}"
 
   end
 
